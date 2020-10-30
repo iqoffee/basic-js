a12212139b97045798040e9bd83a7954831ba9fc
iqoffee <iqoffee@users.noreply.github.com>
2020-10-30 18:29:50 +0500

    end code all withou vigenere-cipher

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(matrix) {
+  if (typeof(matrix) != "object" || matrix == null || matrix[0] == undefined){ return false }
+  arr = matrix.filter(a => typeof(a) === "string");
+  if (arr.join() == ''){return false}
+  let test = arr.map((a) => a.match(/\w+/)).join();
+  test = test.match(/\b[a-zA-Z]/g).map((a)=> a.toUpperCase())
+  return test.sort().join("")
 };
